e3ec1c6333095f3bcc272195549e7170df1cbc56
iimagii <mag_1@ukr.net>
2022-08-31 18:20:40 +0300

    customs hooks - useInput,useHover

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Hover from './components/Hover'
+
+function App() {
+   return (
+      <div className='App'>
+         <Hover />
+      </div>
+   )
+}
+
+export default App
